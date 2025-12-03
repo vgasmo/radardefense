dc18e2de4db5af50b47dc52f9f68a48c7fec3afb
vgasmo <vitor.ferreira@startupleiria.com>
2025-12-03 17:34:55 +0000

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/radardefense/',  // 👈 nome do repositório
+  base: '/radardefense/', // o NOME do repositório
 })
