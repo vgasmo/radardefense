7ae6cdc1354b723e29196d0fcc99908e450e64a9
vgasmo <vitor.ferreira@startupleiria.com>
2025-12-03 17:45:36 +0000

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/radardefense/', // o NOME do repositório
+  base: '/radardefense/', // nome do repositório
 })
